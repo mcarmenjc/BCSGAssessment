04905a64efdb0d4c637c23290078d1de8429555c
mcarmenjimenezcampos <mcarmen.jimenez.campos@onalytica.com>
2015-09-17 17:43:46 +0100

    Created repository constructor to read data from file and parse dates

--- src/bcsg/repositories/BankCardRepository.java
+++ src/bcsg/repositories/BankCardRepository.java
@@ -1,5 +1,46 @@
 package bcsg.repositories;
 
-public class BankCardRepository {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 
+import bcsg.entities.BankCard;
+import bcsg.entities.BankCardFactory;
+
+public class BankCardRepository {
+	private List<BankCard> cards;
+	
+	public BankCardRepository(String fileName){
+		cards = new ArrayList<BankCard>();
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)))
+		{
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] cardData = line.split(";");
+				GregorianCalendar date = ParseCardExpiryDate(cardData);
+				cards.add(BankCardFactory.getBankCard(cardData[1], date, cardData[0]));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+
+	private GregorianCalendar ParseCardExpiryDate(String[] cardData) throws ParseException {
+		DateFormat dateFormat = new SimpleDateFormat("MMM-yyyy");
+		Date parsed = dateFormat.parse(cardData[2]);
+		GregorianCalendar date = new GregorianCalendar();
+		date.setTime(parsed);
+		return date;
+	}
+	
+	public List<BankCard> getAllCardsOrderedByExpiryDate(){
+		return cards;
+	}
 }
