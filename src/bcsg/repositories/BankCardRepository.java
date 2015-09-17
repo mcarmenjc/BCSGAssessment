b4ec15169852f158d191114bd92498d40bd04978
mcarmenjimenezcampos <mcarmen.jimenez.campos@onalytica.com>
2015-09-17 17:50:18 +0100

    reorganised code and sorted list by expiry date using lambda expressions

--- src/bcsg/repositories/BankCardRepository.java
+++ src/bcsg/repositories/BankCardRepository.java
@@ -14,22 +14,10 @@ import bcsg.entities.BankCard;
 import bcsg.entities.BankCardFactory;
 
 public class BankCardRepository {
-	private List<BankCard> cards;
+	private String fileName;
 	
 	public BankCardRepository(String fileName){
-		cards = new ArrayList<BankCard>();
-		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)))
-		{
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				String[] cardData = line.split(";");
-				GregorianCalendar date = ParseCardExpiryDate(cardData);
-				cards.add(BankCardFactory.getBankCard(cardData[1], date, cardData[0]));
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+		this.fileName = fileName;
 	}
 
 	private GregorianCalendar ParseCardExpiryDate(String[] cardData) throws ParseException {
@@ -41,6 +29,20 @@ public class BankCardRepository {
 	}
 	
 	public List<BankCard> getAllCardsOrderedByExpiryDate(){
+		List<BankCard> cards = new ArrayList<BankCard>();
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)))
+		{
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] cardData = line.split(";");
+				GregorianCalendar date = ParseCardExpiryDate(cardData);
+				cards.add(BankCardFactory.getBankCard(cardData[1], date, cardData[0]));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		cards.sort((card1, card2) -> card1.getExpiryDate().compareTo(card2.getExpiryDate()));
 		return cards;
 	}
 }
